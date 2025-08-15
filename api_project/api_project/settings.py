7bdd40dbea753e23bf67f1f7bd54f4a0dc5b8fd3
Haruna Hafiz <harunahafiz1980@gmail.com>
2025-08-15 21:48:21 +0100

    03

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -137,5 +137,8 @@ access certain views or perform certain actions.
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # require login by default
     ]
 }
