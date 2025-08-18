baafe4f4e0df588b7bcaf9951eb63bde82dff520
Haruna Hafiz <harunahafiz1980@gmail.com>
2025-08-18 09:16:06 +0100

    00

--- advanced-api-project/advanced_api_project/api/admin.py
+++ advanced-api-project/advanced_api_project/api/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from .models import Book, Author
 # Register your models here.
 
-@admin.register(Book)
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author","publication_year")   # show these fields in list view
     search_fields = ("title",)                               # add search bar
     list_filter = ("publication_year", "author") 
+    
 
-@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
+
+    
+    
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
